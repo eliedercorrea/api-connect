62cd7dbc7b2a5132edb9dfa6c857a9d952620e83
eliedercorrea <elieder.correa@cs.cruzeirodosul.edu.br>
2026-08-03 00:28:21 -0300

    Adiciona persistência provisória em memória

---
+++ src/data/users.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    id: 1,
+    nome: 'João Silva',
+    email: 'joao@email.com'
+  },
+  {
+    id: 2,
+    nome: 'Maria Souza',
+    email: 'maria@email.com'
+  }
+];
+
+module.exports = users;
